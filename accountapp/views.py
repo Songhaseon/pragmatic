977c372305eb32019f6531b9155615f9a25d3805
songs <lovehasuni@kakao.com>
2023-05-15 18:59:58 +0900

    django course 9

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
     # return HttpResponse("<h1>안녕하세요. 나는 송하선입니다. 당신을 만나 반갑습니다.!</h1>")
