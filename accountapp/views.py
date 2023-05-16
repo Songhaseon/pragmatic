15664639d50cd7163ab3847c543d20a152731362
songs <lovehasuni@kakao.com>
2023-05-16 16:05:47 +0900

    django course 16 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': request.POST.get('input_text')})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
     # return HttpResponse("<h1>안녕하세요. 나는 송하선입니다. 당신을 만나 반갑습니다.!</h1>")
