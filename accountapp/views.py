1c47bc5a683e0642722e155fac5806ef475bdf6f
songs <lovehasuni@kakao.com>
2023-05-16 18:26:02 +0900

    django course 18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,19 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
 
     if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        if request.POST.get('hello_world_input') == '':
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            temp = request.POST.get('hello_world_input')
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            # hello_world_list = HelloWorld.objects.all()
+            # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     # return HttpResponse("<h1>안녕하세요. 나는 송하선입니다. 당신을 만나 반갑습니다.!</h1>")
